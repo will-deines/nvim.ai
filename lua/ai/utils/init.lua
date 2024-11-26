3dbc8468c46e63a9850c62f46cde515c5a49d571
willdeines <wdeines@gmail.com>
2024-11-25 22:18:04 -0500

    add a newline to get loading sep

--- lua/ai/utils/init.lua
+++ lua/ai/utils/init.lua
@@ -277,6 +277,8 @@ M.start_loading = function(win_id, buf_id)
   M.state.loading = true
   M.state.loading_timer = vim.uv.new_timer()
   if M.state.loading_timer then
+    -- Add empty line before spinner
+    vim.api.nvim_buf_set_lines(buf_id, -1, -1, false, { "" })
     M.state.spinner_line = vim.api.nvim_buf_line_count(buf_id)
     vim.api.nvim_buf_set_lines(
       buf_id,
