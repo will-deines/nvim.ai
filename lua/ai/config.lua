c0fc4663412a29a47cec31e3c687f873d700403d
willdeines <wdeines@gmail.com>
2024-12-17 15:40:28 -0500

    pP

--- lua/ai/config.lua
+++ lua/ai/config.lua
@@ -29,7 +29,7 @@ M.defaults = {
     respect_gitignore = true,
     debug = false,
   },
-  debug = true,
+  debug = false,
   -- Chat Dialog UI configuration
   ui = {
     width = 80, -- Width of the chat dialog window
@@ -184,59 +184,61 @@ function M.get(what)
   return M.config[what]
 end
 function M.setup_cmp()
-  local cmp = require("blink.cmp")
-  if cmp and cmp.setup then
-    cmp.setup({
-      snippets = {
-        expand = function(snippet, _)
-          return LazyVim.cmp.expand(snippet)
-        end,
-      },
-      appearance = {
-        -- sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release, assuming themes add support
-        use_nvim_cmp_as_default = false,
-        -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- adjusts spacing to ensure icons are aligned
-        nerd_font_variant = "mono",
-      },
-      completion = {
-        accept = {
-          -- experimental auto-brackets support
-          auto_brackets = {
-            enabled = true,
+  vim.schedule(function()
+    local cmp = require("blink.cmp")
+    if cmp and cmp.setup then
+      cmp.setup({
+        snippets = {
+          expand = function(snippet, _)
+            return LazyVim.cmp.expand(snippet)
+          end,
+        },
+        appearance = {
+          -- sets the fallback highlight groups to nvim-cmp's highlight groups
+          -- useful for when your theme doesn't support blink.cmp
+          -- will be removed in a future release, assuming themes add support
+          use_nvim_cmp_as_default = false,
+          -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+          -- adjusts spacing to ensure icons are aligned
+          nerd_font_variant = "mono",
+        },
+        completion = {
+          accept = {
+            -- experimental auto-brackets support
+            auto_brackets = {
+              enabled = true,
+            },
+          },
+          menu = {
+            draw = {
+              treesitter = { "lsp" },
+            },
+          },
+          documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 200,
+          },
+          ghost_text = {
+            enabled = vim.g.ai_cmp,
           },
         },
-        menu = {
-          draw = {
-            treesitter = { "lsp" },
-          },
+        -- experimental signature help support
+        -- signature = { enabled = true },
+        sources = {
+          -- adding any nvim-cmp sources here will enable them
+          -- with blink.compat
+          compat = {},
+          default = { "lsp", "path", "snippets", "buffer" },
+          cmdline = {},
         },
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 200,
+        keymap = {
+          preset = "enter",
+          ["<C-y>"] = { "select_and_accept" },
         },
-        ghost_text = {
-          enabled = vim.g.ai_cmp,
-        },
-      },
-      -- experimental signature help support
-      -- signature = { enabled = true },
-      sources = {
-        -- adding any nvim-cmp sources here will enable them
-        -- with blink.compat
-        compat = {},
-        default = { "lsp", "path", "snippets", "buffer" },
-        cmdline = {},
-      },
-      keymap = {
-        preset = "enter",
-        ["<C-y>"] = { "select_and_accept" },
-      },
-    })
-  else
-    print("blink.cmp not available")
-  end
+      })
+    else
+      vim.notify("blink.cmp not available", vim.log.levels.ERROR)
+    end
+  end)
 end
 return M
